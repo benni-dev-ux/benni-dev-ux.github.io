ea32b5b5c79ed5715442e7ccdd357675135841d8
Benedikt <benediktsteinmueller@googlemail.com>
2022-10-20 15:05:50 +0200

    fixed scroll button behaviour

--- js/topscroll.js
+++ js/topscroll.js
@@ -1,30 +1,25 @@
 window.onload = function() {
     let top_button = document.getElementById("top_button");
 
-};
+    // When the user scrolls down 20px from the top of the document, show the button
+    window.onscroll = function() {
+        scrollFunction();
+    };
 
-
-// Get the button:
-
-
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() { scrollFunction() };
-
-function scrollFunction() {
-
-    console.log("test")
-    if (document.body.scrollTop > 900 || document.documentElement.scrollTop > 900) {
-        if (document.body.scrollTop > 900 || document.documentElement.scrollTop > 900) {
-            top_button.style.display = "block";  
+    function scrollFunction() {
+        if (
+            document.body.scrollTop > 900 ||
+            document.documentElement.scrollTop > 900
+        ) {
+            top_button.style.display = "block";
         } else {
-            top_button.style.display = "none";  
+            top_button.style.display = "none";
         }
     }
-}
 
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-    document.body.scrollTop = 0; // For Safari
-     
-    document.documentElement.scrollTop = 0;
-}
+    // When the user clicks on the button, scroll to the top of the document
+    function topFunction() {
+        document.body.scrollTop = 0; // For Safari
+        document.documentElement.scrollTop = 0;
+    }
+};
